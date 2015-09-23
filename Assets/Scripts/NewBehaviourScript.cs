bbb6b7571fe14c2e66e77819e721e7a741c80e80
CJ Currie <cj@cjcurrie.net>
2015-09-23 11:58:29 -0600

    Cleaned up .gitignore and removed unnecessary files

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Copyright (c) 2015 Colin James Currie.
+ * All rights reserved.
+ * Contact: cj@cjcurrie.net
+ */
+
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{ 
+  void Update ()
+  {
+    Debug.Log(Input.GetAxis("Mouse X"));
+  }
+}
